a687a9679e229f0b7fac8d3e0227da52f8b9dd4f
Ettore Caprioli <ettore.caprioli@gmail>
2021-08-29 20:20:54 +0200

    add marshall and unmarshall of json

--- game/game.go
+++ game/game.go
@@ -15,8 +15,55 @@ limitations under the License.
 */
 package game
 
-func RunQuiz(quiz string) string {
-    
-    return quiz
+import (
+    "encoding/json"
+    "fmt"
+//    "os"
+)
+
+type question_st struct {
+    Name        string  `json:"name"`
+    Text        string  `json:"text"`
+    Answers   []string  `json:"answers"`
 }
 
+type quiz_st struct {
+    Questions   []question_st     `json:"questions"`
+}
+
+
+func RunQuiz(quiz_str string) string {
+    
+    var quiz quiz_st
+    quiz = quiz_str2structure(quiz_str)
+    fmt.Println(string(quiz.Questions[0].Name))
+    
+    var ans = make(map[string]string)
+    ans["capital-of-italy"]="Rome"
+    ans["weather-in-malta"]="Sunny"
+
+    return answer2str(ans)
+/*
+    enc := json.NewEncoder(os.Stdout)
+    d := map[string]string{"apple": "baluba", "lettuce": "7"}
+    enc.Encode(d)
+
+    var try map[string]string
+    json.Unmarshal([]byte("{\"ciao\":\"maio\",\"bau\":\"cane\"}"), &try)
+    fmt.Println(try["ciao"])
+    fmt.Println(try["bau"])
+*/
+}
+
+func quiz_str2structure(quiz_str string) quiz_st {
+    res := quiz_st{}
+    json.Unmarshal([]byte(quiz_str), &res)
+    //fmt.Println(res)
+    //fmt.Println(res.Questions[0].Name)
+    return res
+}
+
+func answer2str(ans map[string]string) string {
+    ans_json, _ := json.Marshal(ans)
+    return string(ans_json)
+}
